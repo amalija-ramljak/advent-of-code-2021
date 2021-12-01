d1f0ef994d8a35fea02d4ae4bf21290d880f3127
amalija-ramljak <amalija.ramljak@gmail.com>
2021-12-01 23:25:32 +0100

    add boilerplate

---
+++ day16/index.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const fileContent = fs.readFileSync('day16/puzzleInput.txt', { encoding: 'utf8', flag: 'r' });
+
+// Part 1 variables
+let result1;
+
+// Part 2 variables
+let result2;
+
+
+
+console.log(`Day 2 / Part 1\n\tResult: ${result1}`);
+console.log(`Day 2 / Part 2\n\tResult: ${result2}`);
