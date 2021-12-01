27d35776d06c52a8d2c737dec70221176921c334
Amalija Ramljak <amalija@netgen.io>
2021-12-01 20:16:25 +0100

    01-2 solution

--- day01/index.ts
+++ day01/index.ts
@@ -1,15 +1,33 @@
 import fs from 'fs';
+import { arraySum } from '../utils/utils';
 
-const fileContent = fs.readFileSync('day01/puzzleInput.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
+const puzzleInput = fs.readFileSync('day01/puzzleInput.txt', { encoding: 'utf8', flag: 'r' }).split('\n').map((v) => parseInt(v));
 
 // Part 1 variables
 let localIncreases = 0;
 
-fileContent.reduce((previousDepth, currentDepth) => {
-    if (parseInt(currentDepth) > parseInt(previousDepth)) {
+// Part 2 variables
+let previousWindow = -1;
+let currentWindow = -1;
+let windowIncreases = 0;
+
+puzzleInput.forEach((currentDepth, index, depthList) => {
+    if (index > 0 && currentDepth > depthList[index - 1]) {
         localIncreases++;
     }
-    return currentDepth;
+
+    if (index > depthList.length - 2) return;
+
+    if (previousWindow === -1) {
+        previousWindow = arraySum(depthList.slice(index, index + 3));
+    } else {
+        currentWindow = arraySum(depthList.slice(index, index + 3));
+        if (currentWindow > previousWindow) {
+            windowIncreases++;
+        }
+        previousWindow = currentWindow;
+    }
 })
 
 console.log(`Day 1 / Part 1\n\tResult: ${localIncreases}`);
+console.log(`Day 1 / Part 2\n\tResult: ${windowIncreases}`);
