ef77ce263ff995ed5857a4d220b0e957821b5b9f
amalija-ramljak <amalija.ramljak@gmail.com>
2021-12-05 10:34:10 +0100

    05-2 solution + refactoring of 05-1

--- day05/index.ts
+++ day05/index.ts
@@ -33,36 +33,52 @@ puzzleInput.forEach((line_definition) => {
 
 type Overlaps = Record<string, number>;
 const get_overlap_key = (point: Point): string => `${point.x},${point.y}`;
-const smaller_bigger_h_point = (point1: Point, point2: Point): Point[] =>
-  point1.y < point2.y ? [point1, point2] : [point2, point1];
-const smaller_bigger_v_point = (point1: Point, point2: Point): Point[] =>
-  point1.x < point2.x ? [point1, point2] : [point2, point1];
+const get_direction_vector = (start: Point, end: Point): Point => {
+  return {
+    x: (end.x - start.x) / (Math.abs(end.x - start.x) || 1),
+    y: (end.y - start.y) / (Math.abs(end.y - start.y) || 1),
+  };
+};
+
+const for_condition = (p: Point, direction: Point, end: Point): boolean => {
+  if ((direction.x < 0 && p.x < end.x) || (direction.x > 0 && p.x > end.x)) {
+    return false;
+  }
+  if ((direction.y < 0 && p.y < end.y) || (direction.y > 0 && p.y > end.y)) {
+    return false;
+  }
+  return true;
+};
 
 // Part 1 variables
 let result1 = 0;
-const lines_1 = lines.filter((line) => line.start.x === line.end.x || line.start.y === line.end.y);
+// const lines_1 = lines.filter((line) => line.start.x === line.end.x || line.start.y === line.end.y);
 const overlaps_1: Overlaps = {};
 
 // Part 2 variables
-let result2;
+let result2 = 0;
+const overlaps_2: Overlaps = {};
 
-lines_1.forEach((line) => {
-  const isHorizontal = line.start.x === line.end.x;
-  const isVertical = line.start.y === line.end.y;
-  const [start, end] = isHorizontal
-    ? smaller_bigger_h_point(line.start, line.end)
-    : smaller_bigger_v_point(line.start, line.end);
+lines.forEach((line) => {
+  const direction = get_direction_vector(line.start, line.end);
 
-  let p = isHorizontal ? start.y : start.x;
-  for (p; isHorizontal ? p <= end.y : p <= end.x; p++) {
-    const overlap_point_key = get_overlap_key({
-      x: isHorizontal ? start.x : p,
-      y: isHorizontal ? p : start.y,
-    });
-    if (overlaps_1[overlap_point_key] !== undefined) {
-      overlaps_1[overlap_point_key]++;
+  for (
+    let p = { ...line.start };
+    for_condition(p, direction, line.end);
+    p.x += direction.x, p.y += direction.y
+  ) {
+    const overlap_point_key = get_overlap_key(p);
+    if (line.start.x === line.end.x || line.start.y === line.end.y) {
+      if (overlaps_1[overlap_point_key] !== undefined) {
+        overlaps_1[overlap_point_key]++;
+      } else {
+        overlaps_1[overlap_point_key] = 1;
+      }
+    }
+    if (overlaps_2[overlap_point_key] !== undefined) {
+      overlaps_2[overlap_point_key]++;
     } else {
-      overlaps_1[overlap_point_key] = 1;
+      overlaps_2[overlap_point_key] = 1;
     }
   }
 });
@@ -70,6 +86,12 @@ lines_1.forEach((line) => {
 for (let overlap_count of Object.values(overlaps_1)) {
   if (overlap_count >= 2) {
     result1++;
+  }
+}
+
+for (let overlap_count of Object.values(overlaps_2)) {
+  if (overlap_count >= 2) {
+    result2++;
   }
 }
 
