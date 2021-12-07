10fb310bd046ccc3d16dfb210fdb688c8a2595da
amalija-ramljak <amalija.ramljak@gmail.com>
2021-12-07 16:23:50 +0100

    07 solutions

--- day07/index.ts
+++ day07/index.ts
@@ -1,14 +1,28 @@
 import fs from 'fs';
 
-const fileContent = fs.readFileSync('day07/puzzleInput.txt', { encoding: 'utf8', flag: 'r' });
+const puzzleInput = fs
+  .readFileSync('day07/puzzleInput.txt', { encoding: 'utf8', flag: 'r' })
+  .split(',')
+  .map(e => parseInt(e));
+
+puzzleInput.sort((a, b) => a - b);
 
 // Part 1 variables
-let result1;
+let result1 = 0;
 
 // Part 2 variables
-let result2;
+let result2 = 0;
 
+const mean_position = puzzleInput[Math.floor(puzzleInput.length / 2)];
+const average_position = Math.floor(puzzleInput.reduce((p, c) => p + c) / puzzleInput.length);
+let diff;
+puzzleInput.forEach(crab_position => {
+    diff = Math.abs(crab_position - mean_position);
+    result1 += diff;
+    
+    diff = Math.abs(crab_position - average_position);
+    result2 += (diff * (diff + 1)) / 2;
+})
 
-
-console.log(`Day 2 / Part 1\n\tResult: ${result1}`);
-console.log(`Day 2 / Part 2\n\tResult: ${result2}`);
+console.log(`Day 7 / Part 1\n\tResult: ${result1}`);
+console.log(`Day 7 / Part 2\n\tResult: ${result2}`);
