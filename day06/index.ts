260b2bc3545726451c41520ce04e1a1fcfe707bd
amalija-ramljak <amalija.ramljak@gmail.com>
2021-12-07 15:36:04 +0100

    06-2 refactor into more efficient solution for part 2

--- day06/index.ts
+++ day06/index.ts
@@ -1,6 +1,9 @@
 import fs from 'fs';
 
-const fileContent = fs.readFileSync('day06/puzzleInput.txt', { encoding: 'utf8', flag: 'r' });
+const fileContent = fs
+  .readFileSync('day06/puzzleInput.txt', { encoding: 'utf8', flag: 'r' })
+  .split(',')
+  .map((e) => parseInt(e));
 
 // Part 1 variables
 let result1;
@@ -8,7 +11,31 @@ let result1;
 // Part 2 variables
 let result2;
 
+let current_length = fileContent.length;
 
+// 0, 1, 2, 3, 4, 5, 6, 7, 8
+let fish_counters = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+fileContent.forEach((fish) => {
+  fish_counters[fish]++;
+});
+
+let new_counters = [...fish_counters];
+
+for (let day = 1; day <= 256; day++) {
+  if (day === 81) {
+    result1 = fish_counters.reduce((p, c) => p + c);
+  }
+  fish_counters.forEach((no_of_fish, idx) => {
+    new_counters[!idx ? 6 : idx - 1] += no_of_fish;
+    new_counters[idx] -= no_of_fish;
+
+    if (idx === 0) {
+      new_counters[8] += no_of_fish;
+    }
+  });
+  fish_counters = [...new_counters];
+}
+result2 = fish_counters.reduce((p, c) => p + c);
 
 console.log(`Day 2 / Part 1\n\tResult: ${result1}`);
 console.log(`Day 2 / Part 2\n\tResult: ${result2}`);
