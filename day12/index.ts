ba53033d100dc733de4072cd469b67761fda7d84
amalija-ramljak <amalija.ramljak@gmail.com>
2021-12-12 11:09:20 +0100

    12-1 solution

--- day12/index.ts
+++ day12/index.ts
@@ -4,24 +4,43 @@ const puzzleInput = fs
   .readFileSync('day12/my.puzzle.txt', { encoding: 'utf8', flag: 'r' })
   .split('\n');
 
-const caveSystem: Record<string, string[]> = {};
+const cave_system: Record<string, string[]> = {};
 
 puzzleInput.forEach((passage) => {
   const [cave_1, cave_2] = passage.split('-');
-  if (caveSystem[cave_1]) {
-    caveSystem[cave_1].push(cave_2);
+  if (cave_system[cave_1]) {
+    cave_system[cave_1].push(cave_2);
   } else {
-    caveSystem[cave_1] = [cave_2];
+    cave_system[cave_1] = [cave_2];
   }
-  if (caveSystem[cave_2]) {
-    caveSystem[cave_2].push(cave_1);
+  if (cave_system[cave_2]) {
+    cave_system[cave_2].push(cave_1);
   } else {
-    caveSystem[cave_2] = [cave_1];
+    cave_system[cave_2] = [cave_1];
   }
 });
 
 const isBig = (cave: string): boolean => {
   return cave.toUpperCase() === cave;
+};
+
+const START = 'start';
+const END = 'end';
+
+const paths = [];
+
+const findPaths = (current_position: string, parent_path: string[]) => {
+  if (current_position === END) {
+    paths.push(parent_path);
+    return;
+  }
+
+  for (let neighbour_cave of cave_system[current_position]) {
+    if (neighbour_cave === START) continue;
+    if (isBig(neighbour_cave) || !parent_path.includes(neighbour_cave)) {
+      findPaths(neighbour_cave, parent_path.concat([neighbour_cave]));
+    }
+  }
 };
 
 // Part 1 variables
@@ -30,7 +49,9 @@ let result1;
 // Part 2 variables
 let result2;
 
+findPaths(START, [START]);
 
+result1 = paths.length;
 
 console.log(`Day 12 / Part 1\n\tResult: ${result1}`);
 console.log(`Day 12 / Part 2\n\tResult: ${result2}`);
