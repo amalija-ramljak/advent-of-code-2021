7d7d392ca999e6288c00b4a4f20ad1e864dc6bbd
amalija-ramljak <amalija.ramljak@gmail.com>
2021-12-12 10:49:51 +0100

    12-1 setup

--- day12/index.ts
+++ day12/index.ts
@@ -1,6 +1,28 @@
 import fs from 'fs';
 
-const fileContent = fs.readFileSync('day12/puzzleInput.txt', { encoding: 'utf8', flag: 'r' });
+const puzzleInput = fs
+  .readFileSync('day12/my.puzzle.txt', { encoding: 'utf8', flag: 'r' })
+  .split('\n');
+
+const caveSystem: Record<string, string[]> = {};
+
+puzzleInput.forEach((passage) => {
+  const [cave_1, cave_2] = passage.split('-');
+  if (caveSystem[cave_1]) {
+    caveSystem[cave_1].push(cave_2);
+  } else {
+    caveSystem[cave_1] = [cave_2];
+  }
+  if (caveSystem[cave_2]) {
+    caveSystem[cave_2].push(cave_1);
+  } else {
+    caveSystem[cave_2] = [cave_1];
+  }
+});
+
+const isBig = (cave: string): boolean => {
+  return cave.toUpperCase() === cave;
+};
 
 // Part 1 variables
 let result1;
@@ -10,5 +32,5 @@ let result2;
 
 
 
-console.log(`Day 2 / Part 1\n\tResult: ${result1}`);
-console.log(`Day 2 / Part 2\n\tResult: ${result2}`);
+console.log(`Day 12 / Part 1\n\tResult: ${result1}`);
+console.log(`Day 12 / Part 2\n\tResult: ${result2}`);
