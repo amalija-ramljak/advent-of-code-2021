3d0c2ac1585ff7fa3af9884d1cebffd8de95b51d
amalija-ramljak <amalija.ramljak@gmail.com>
2021-12-13 22:13:46 +0100

    13 solutions

--- day13/index.ts
+++ day13/index.ts
@@ -1,14 +1,70 @@
 import fs from 'fs';
 
-const fileContent = fs.readFileSync('day13/puzzleInput.txt', { encoding: 'utf8', flag: 'r' });
+const [dots, folds] = fs
+  .readFileSync('day13/my.puzzle', { encoding: 'utf8', flag: 'r' })
+  .split('\n\n')
+  .map((section, idx) => {
+    if (idx === 0) {
+      return section.split('\n').map((pt) => {
+        let [x, y] = pt.split(',');
+        return { x: parseInt(x), y: parseInt(y) };
+      });
+    } else {
+      return section.split('\n').map((fold) => {
+        const [axis, value] = fold.split(' ')[2].split('=');
+        return {
+          x: axis === 'x' ? parseInt(value) : 0,
+          y: axis === 'y' ? parseInt(value) : 0,
+        };
+      });
+    }
+  });
 
 // Part 1 variables
 let result1;
 
 // Part 2 variables
-let result2;
+let result2 = '';
 
+type Dot = { x: number; y: number };
+let currentDots: Dot[] = dots.slice();
+let newDots: Dot[] = [];
+let foldStep = 0;
+for (let fold of folds) {
+  newDots = [];
+  currentDots.forEach((dot) => {
+    if (fold.x !== 0 && dot.x > fold.x) {
+      dot.x -= 2 * Math.abs(dot.x - fold.x);
+    } else if (fold.y !== 0 && dot.y > fold.y) {
+      dot.y -= 2 * Math.abs(dot.y - fold.y);
+    }
+    if (!newDots.find((newDot) => newDot.x === dot.x && newDot.y === dot.y)) {
+      newDots.push(dot);
+    }
+  });
+  currentDots = newDots;
+  foldStep++;
+  if (foldStep === 1) {
+    result1 = currentDots.length;
+  }
+}
 
+console.log(`Day 13 / Part 1\n\tResult: ${result1}`);
+console.log('Day 13 / Part 2\n\tResult: in file day13/part2.output');
 
-console.log(`Day 2 / Part 1\n\tResult: ${result1}`);
-console.log(`Day 2 / Part 2\n\tResult: ${result2}`);
+let max_x = 0, max_y = 0;
+currentDots.forEach(dot => {
+    if (dot.x > max_x) max_x = dot.x;
+    if (dot.y > max_y) max_y = dot.y;
+});
+for (let row = 0; row <= max_y; row++) {
+  for (let col = 0; col <= max_x; col++) {
+    if (!currentDots.find((dot) => dot.x === col && dot.y === row)) {
+      result2 += '.';
+    } else {
+      result2 += '#';
+    }
+  }
+  result2 += '\n';
+}
+fs.writeFileSync('day13/part2.output', result2);
